f6755a3af9535244db23041e621eecea6e1578a3
wcx <wcx@yitudevdeiMac.local>
2017-09-28 18:14:44 +0800

    新建工程

---
+++ TestGit/TestGit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestGit
+//
+//  Created by wcx on 2017/9/28.
+//  Copyright © 2017年 wcx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
